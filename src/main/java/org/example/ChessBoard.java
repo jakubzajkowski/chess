4ddd5a023563a1f6d6c30fb90f4aed01ed7c07bd
Jakub Zajkowski <jakudjan123@gmail.com>
2024-04-11 14:08:26 +0200

    feat: Implment padding to pawns

--- src/main/java/org/example/ChessBoard.java
+++ src/main/java/org/example/ChessBoard.java
@@ -8,13 +8,20 @@ import java.util.List;
 public class ChessBoard extends JPanel {
     private final int BOARD_SIZE = 8;
     private final int SQUARE_SIZE = 100;
+    private Rectangle[][] squares = new Rectangle[BOARD_SIZE][BOARD_SIZE];;
 
     private List<ChessPawn> pawnList = new ArrayList<>();
 
     public ChessBoard() {
         this.setPreferredSize(new Dimension(BOARD_SIZE*SQUARE_SIZE,BOARD_SIZE*SQUARE_SIZE));
-        ChessPawn pawn = new ChessPawn(1);
-        pawnList.add(pawn);
+        for (int i=0;i<this.BOARD_SIZE;i++){
+            ChessPawn pawn = new ChessPawn(2,0);
+            pawnList.add(pawn);
+        }
+        for (int i=0;i<this.BOARD_SIZE;i++){
+            ChessPawn pawn = new ChessPawn(2,1);
+            pawnList.add(pawn);
+        }
     }
     public void paint(Graphics g) {
         for (int row = 0; row < BOARD_SIZE; row++) {
@@ -22,12 +29,21 @@ public class ChessBoard extends JPanel {
                 int x = col * SQUARE_SIZE;
                 int y = row * SQUARE_SIZE;
 
-                if ((row + col) % 2 == 0) g.setColor(Color.GRAY);
-                else g.setColor(Color.RED);
+                int padding = (int) (0.1 * SQUARE_SIZE);
+
+                squares[row][col] = new Rectangle(x, y, SQUARE_SIZE, SQUARE_SIZE);
+
+                if ((row + col) % 2 == 0) g.setColor(Color.getHSBColor(0.25F, 1.0F, 0.55F));
+                else g.setColor(Color.getHSBColor(0.25F, 0.10F, 0.85F));
 
                 g.fillRect(x, y, SQUARE_SIZE, SQUARE_SIZE);
                 g.fillRect(x, y, SQUARE_SIZE, SQUARE_SIZE);
-                g.drawImage(this.pawnList.get(0).image, x, y, SQUARE_SIZE, SQUARE_SIZE, null);
+                if (row==1){
+                    g.drawImage(this.pawnList.get(row).image, x + padding, y + padding, SQUARE_SIZE - 2 * padding, SQUARE_SIZE - 2 * padding, null);
+                }
+                if (row==6){
+                    g.drawImage(this.pawnList.get(row+7).image, x + padding, y + padding, SQUARE_SIZE - 2 * padding, SQUARE_SIZE - 2 * padding, null);
+                }
             }
         }
     }
--- src/main/java/org/example/ChessPawn.java
+++ src/main/java/org/example/ChessPawn.java
@@ -2,26 +2,27 @@ package org.example;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 
 public class ChessPawn {
-    public final int None = 0;
     public final int King = 1;
     public final int Pawn = 2;
     public final int Knight = 3;
     public final int Bishop = 4;
     public final int Rook = 5;
     public final int Queen = 6;
+    public final int White = 1;
+    public final int Black = 0;
 
     public BufferedImage image;
 
-    public ChessPawn(int pawn){
-        if (pawn==1){
-            this.image=loadImage("/pawn.png");
+    public ChessPawn(int pawn,int color){
+        if (pawn==2 && color==0){
+            this.image=loadImage("/pawn_black.png");
+        }
+        if (pawn==2 && color==1){
+            this.image=loadImage("/pawn_white.png");
         }
     }
-
-
     private BufferedImage loadImage(String path) {
         try {
             return ImageIO.read(getClass().getResource(path));
