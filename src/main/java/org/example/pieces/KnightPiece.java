2cb8528e1f6f0c3de3712107e47c8120249eece3
Jakub Zajkowski <jakudjan123@gmail.com>
2024-04-28 13:17:56 +0200

    feat: Implement knight moves and kill logic

--- src/main/java/org/example/pieces/KnightPiece.java
+++ src/main/java/org/example/pieces/KnightPiece.java
@@ -9,6 +9,25 @@ public class KnightPiece extends ChessPiece {
 
     @Override
     public boolean isValidMove(int startX, int startY, int x, int y, ChessPiece[][] board) {
-       return false;
+        if (x < 0 || x >= 8 || y < 0 || y >= 8 || (startX == x && startY == y)) {
+            return false;
+        }
+
+        int deltaX = Math.abs(x - startX);
+        int deltaY = Math.abs(y - startY);
+
+        if ((deltaX == 2 && deltaY == 1) || (deltaX == 1 && deltaY == 2)) {
+            if (board[y][x] != null && board[y][x].color == board[startY][startX].color) {
+                return false;
+            }
+
+            if (board[y][x] != null && board[y][x].color != board[startY][startX].color) {
+                kill(board, x, y);
+            }
+
+            return true;
+        }
+
+        return false;
     }
 }
